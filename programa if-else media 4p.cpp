8d27cacb9efcc7fb3c1a399ae6e11aa00670b72a
Lucas Rodrigues Pereira <lucasrpereira29@gmail.com>
2022-03-22 15:03:12 -0300

    Add files via upload

---
+++ programa if-else media 4p.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main()
+
+
+{
+    
+
+double nota1, nota2, nota3, media;
+
+
+    
+    cout<<"insira sua nota 1:" ;
+    
+        cin>> (nota1);
+    
+
+        cout<<"insira sua nota 2:" ;
+    
+        cin>> (nota2);
+    
+
+       cout<<"insira sua nota 3:" ;
+   
+       cin>> (nota3);
+    
+
+    
+       media = (nota3 + nota2 + nota1) / 3;
+    
+   
+ 
+       if (media >= 6)
+       
+     
+               cout <<"voce passou com:"<< media;
+
+       
+      else 
+       
+               cout<<"voce nao passou com:"<< media;
+    
+  
+  
+   
+ return 0;
+
+}
